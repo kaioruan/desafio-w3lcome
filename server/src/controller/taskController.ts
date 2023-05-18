d2e38c501995a6204c4d9398acf5a50b88427262
kaioruan <kaio.ruan@souunit.com.br>
2023-05-18 09:42:42 -0300

    feat: create controller createTask

--- server/src/controller/taskController.ts
+++ server/src/controller/taskController.ts
@@ -10,6 +10,13 @@ class taskController {
     const tasks = await this.taskService.getAllTask();
     return res.status(200).json(tasks);
   };
+
+  public createTask = async (req: Request, res: Response): Promise<Response> => {
+    const { titulo, concluida } = req.body;
+    const task = await this.taskService.createTask(titulo as string, concluida as boolean);
+    if (!task) return res.status(500).json({ message: 'Internal Server Error' });
+    return res.status(201).json(task);
+  };
 }
 
 export default taskController;
